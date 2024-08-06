ac4b48adcd9819b19a14edb3318e01e30687ef8e
madeinspace <fabrice@funkadelik.com>
2024-08-06 14:44:32 +1000

    upd: some refactor

--- src/components/RobotContext.jsx
+++ src/components/RobotContext.jsx
@@ -2,52 +2,61 @@ import React, { createContext, useContext, useState, useCallback } from 'react';
 
 const RobotContext = createContext();
 
+export const useRobot = () => useContext(RobotContext);
+
 export const RobotProvider = ({ children }) => {
   const [robotState, setRobotState] = useState({
     position: { x: 2, y: 2 },
     direction: 'E',
   });
 
+  const getNextDirection = (currentDirection) => {
+    switch (currentDirection) {
+      case 'N':
+        return 'E';
+      case 'E':
+        return 'S';
+      case 'S':
+        return 'W';
+      case 'W':
+        return 'N';
+      default:
+        return 'E';
+    }
+  };
+
+  const getNextPosition = (currentDirection, currentPosition) => {
+    let { x, y } = currentPosition;
+    switch (currentDirection) {
+      case 'N':
+        if (y > 0) y -= 1;
+        break;
+      case 'E':
+        if (x < 4) x += 1;
+        break;
+      case 'S':
+        if (y < 4) y += 1;
+        break;
+      case 'W':
+        if (x > 0) x -= 1;
+        break;
+      default:
+        break;
+    }
+    return {x, y}
+  }
+
   const rotateRobot = () => {
     setRobotState(prevState => {
-      const newDirection = (() => {
-        switch (prevState.direction) {
-          case 'N':
-            return 'E';
-          case 'E':
-            return 'S';
-          case 'S':
-            return 'W';
-          case 'W':
-            return 'N';
-          default:
-            return 'E';
-        }
-      })();
+      const newDirection = getNextDirection(prevState.direction);
       return { ...prevState, direction: newDirection };
     });
   };
 
   const moveRobot = () => {
     setRobotState((prevState) => {
-      let { x, y } = prevState.position;
-      switch (prevState.direction) {
-        case 'N':
-          if (y > 0) y -= 1;
-          break;
-        case 'E':
-          if (x < 4) x += 1;
-          break;
-        case 'S':
-          if (y < 4) y += 1;
-          break;
-        case 'W':
-          if (x > 0) x -= 1;
-          break;
-        default:
-          break;
-      }
-      return { ...prevState, position: { x, y } };
+      const newPosition = getNextPosition(prevState.direction, prevState.position)
+      return { ...prevState, position: newPosition };
     });
   };
 
@@ -115,4 +124,4 @@ export const RobotProvider = ({ children }) => {
   );
 };
 
-export const useRobot = () => useContext(RobotContext);
+
